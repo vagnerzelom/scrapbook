df986116db62233c0469d56c1859e11450e9f8fd
vagnerzelom <vagnerzelom@hotmail.com>
2020-07-02 22:26:40 -0300

    remover salvamento no local store

--- script.js
+++ script.js
@@ -6,32 +6,6 @@ let container = document.querySelector('.container');
 let recados = [] ; 
 
 
-for (const recado of recados) {
-  let scrapField =  document.createElement('div')
-  let botao = document.createElement('button')
-  scrapField.appendChild(botao)
-  botao.innerHTML='Apagar';
-  botao.setAttribute('class', 'btn-apagar')
-  scrapField.setAttribute('class','d-flex mt-5 flex-wrap justify-content-center w-100');
-  scrapField.setAttribute('id','scrapsField');
-  let cartaoMensagem = document.createElement('div');
-  cartaoMensagem.setAttribute('class',"message-cards card text-white bg-dark m-2 col-3");
-  scrapField.appendChild(cartaoMensagem);
-  let tituloCartao = document.createElement('div');
-  tituloCartao.setAttribute('class','card-header font-weight-bold');  
-  tituloCartao.innerHTML= recado[0];
-  cartaoMensagem.appendChild(tituloCartao);
-  let corpoCartao = document.createElement('div');
-  corpoCartao.setAttribute('class','card-body');
-  cartaoMensagem.appendChild(corpoCartao);
-  let textoCartao =document.createElement('p');
-  textoCartao.setAttribute('class','card-text');
-  textoCartao.innerHTML = recado[1];
-  corpoCartao.appendChild(textoCartao);
-  container.appendChild(scrapField);
-  
-};
-
 
 adicionar.onclick = function(){
     if(!titulo.value || !mensagem.value) return;
