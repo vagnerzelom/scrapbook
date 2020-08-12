1eab2a5d17fc057c99717e72bfefd5ffc85009f2
vagnerzelom <vagnerzelom@hotmail.com>
2020-08-11 22:36:45 -0300

    instalação de denpencias e vendo axios

--- public/bundle.js
+++ public/bundle.js
@@ -1,164 +1,113 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
 "use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _soma__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./soma */ \"./src/soma.js\");\n// class TaskList{\n//     constructor(){\n//         this.tituloInput = document.getElementById('titleInput');\n//         this.mensagemInput = document.getElementById('messageField');\n//         this.adicionar = document.getElementById('bnt');\n//         this.caixaRecados= document.getElementById('caixa-recados');\n//         this.editTexto= document.getElementById('editTitleInput');\n//         this.editMessagem = document.getElementById('editMessageField');\n//         this.salveedit= document.getElementById('saveEdit');\n//        this.recados= JSON.parse(localStorage.getItem(\"recados\")) || [];\n//        this.criarRecados();\n//        this.regitaAddbtnEvent();\n//     }\n//     generateRecadoId(){\n//       return this.recados.length +1;\n//     }\n//     regitaAddbtnEvent(){\n//         this.adicionar.onclick= () => this.novaMensagem();\n//     }\n//     botaoEvento(){\n//       document.querySelectorAll('.delete-button').forEach((item)=>{item.onclick=(event)=>this.deletaMenssagem(event);\n//       });\n//       document.querySelectorAll('.editar-button').forEach(item=>{item.onclick = event=> this.editaRecado(event);\n//       });\n//     }\n//     criarRecados(){\n//         this.caixaRecados.innerHTML =\"\";\n//         for (const recado of this.recados) {         \n//         const cardHtml = this.criaCartaoMensagem(\n//         recado.id,\n//         recado.titulo,\n//         recado.mensagem);\n//         this.inserirHtml(cardHtml);\n//       }\n//       this.botaoEvento();\n//      }\n//     novaMensagem(){\n//         if(!this.tituloInput.value || !this.mensagemInput.value){\n//           alert('O titulo e a mensagem deve se digitadas!')\n//           return;} \n//         const id = this.generateRecadoId();\n//         const titulo = this.tituloInput.value;\n//         const mensagem = this.mensagemInput.value;\n//         this.tituloInput.value = '';\n//         this.mensagemInput.value= '';\n//         this.recados.push({id,titulo,mensagem});\n//         this.criarRecados();\n//         this.savarLocalstore();\n//       }\n//       deletaMenssagem(event){\n//        if(!confirm('Deseja realmente apagar esta mensagem?'))return;\n//         event.path[2].remove()\n//       const scrapId = event.path[2].getAttribute('id-scrap');\n//       const scrapIndex =this.recados.findIndex(item=>{\n//         return item.id == scrapId;\n//       })\n//       this.recados.splice(scrapIndex, 1);\n//       this.savarLocalstore();\n//       }\n//       inserirHtml(html){\n//         this.caixaRecados.innerHTML += html\n//       }\n//       editaRecado(event) {\n//         $('#editModal').modal('toggle');\n//         const scrapId = event.path[2].getAttribute('id-scrap');\n//         const scrapIndex = this.recados.findIndex((item) => {\n//           return item.id == scrapId\n//         })\n//          this.editTexto.value = this.recados[scrapIndex].titulo;\n//          this.editMessagem.value = this.recados[scrapIndex].mensagem;\n//         this.salveedit.onclick = () => this.editSalvar(scrapIndex) \n//        }\n//        editSalvar(scrapIndex){\n//         if(!confirm('Você realmente deseja salvar esta mensagem?')) return;\n//         alert('Mensagem salva com sucesso!')\n//         $(\"#editModal\").modal(\"hide\");\n//        this.recados[scrapIndex].titulo = this.editTexto.value;\n//        this.recados[scrapIndex].mensagem = this.editMessagem.value ;\n//        this.criarRecados();\n//        this.savarLocalstore();\n//        }\n//        savarLocalstore(){\n//         localStorage.setItem('recados',JSON.stringify(this.recados));\n//       }\n//     criaCartaoMensagem(id, titulo,mensagem){\n//         return`\n//         <div class=\"message-cards card text-white bg-dark m-2 col-3\" id-scrap=\"${id}\">\n//         <div class=\"card-header font-weight-bold\">${titulo}</div>\n//         <div class=\"card-body\">\n//           <p class=\"card-text\">\n//             ${mensagem}\n//           </p>\n//         </div>\n//         <div class=\"w-100 d-flex justify-content-end pr-2 pb-2\">\n//           <button class=\"btn btn-danger mr-1 delete-button\" \n//           >Apagar</button>\n//           <button class=\"btn btn-info editar-button \">Editar</button>\n//         </div>\n//       </div>\n//       `;}\n// }\n// new TaskList();\n\nalert(Object(_soma__WEBPACK_IMPORTED_MODULE_0__[\"soma\"])(1, 2));\n\n//# sourceURL=webpack:///./src/index.js?");
 
-function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+/***/ }),
 
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+/***/ "./src/soma.js":
+/*!*********************!*\
+  !*** ./src/soma.js ***!
+  \*********************/
+/*! exports provided: soma */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"soma\", function() { return soma; });\nvar soma = function soma(a, b) {\n  return a + b;\n};\n\n//# sourceURL=webpack:///./src/soma.js?");
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+/***/ })
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var TaskList = /*#__PURE__*/function () {
-  function TaskList() {
-    _classCallCheck(this, TaskList);
-
-    this.tituloInput = document.getElementById('titleInput');
-    this.mensagemInput = document.getElementById('messageField');
-    this.adicionar = document.getElementById('bnt');
-    this.caixaRecados = document.getElementById('caixa-recados');
-    this.editTexto = document.getElementById('editTitleInput');
-    this.editMessagem = document.getElementById('editMessageField');
-    this.salveedit = document.getElementById('saveEdit');
-    this.recados = JSON.parse(localStorage.getItem("recados")) || [];
-    this.criarRecados();
-    this.regitaAddbtnEvent();
-  }
-
-  _createClass(TaskList, [{
-    key: "generateRecadoId",
-    value: function generateRecadoId() {
-      return this.recados.length + 1;
-    }
-  }, {
-    key: "regitaAddbtnEvent",
-    value: function regitaAddbtnEvent() {
-      var _this = this;
-
-      this.adicionar.onclick = function () {
-        return _this.novaMensagem();
-      };
-    }
-  }, {
-    key: "botaoEvento",
-    value: function botaoEvento() {
-      var _this2 = this;
-
-      document.querySelectorAll('.delete-button').forEach(function (item) {
-        item.onclick = function (event) {
-          return _this2.deletaMenssagem(event);
-        };
-      });
-      document.querySelectorAll('.editar-button').forEach(function (item) {
-        item.onclick = function (event) {
-          return _this2.editaRecado(event);
-        };
-      });
-    }
-  }, {
-    key: "criarRecados",
-    value: function criarRecados() {
-      this.caixaRecados.innerHTML = "";
-
-      var _iterator = _createForOfIteratorHelper(this.recados),
-          _step;
-
-      try {
-        for (_iterator.s(); !(_step = _iterator.n()).done;) {
-          var recado = _step.value;
-          var cardHtml = this.criaCartaoMensagem(recado.id, recado.titulo, recado.mensagem);
-          this.inserirHtml(cardHtml);
-        }
-      } catch (err) {
-        _iterator.e(err);
-      } finally {
-        _iterator.f();
-      }
-
-      this.botaoEvento();
-    }
-  }, {
-    key: "novaMensagem",
-    value: function novaMensagem() {
-      if (!this.tituloInput.value || !this.mensagemInput.value) {
-        alert('O titulo e a mensagem deve se digitadas!');
-        return;
-      }
-
-      var id = this.generateRecadoId();
-      var titulo = this.tituloInput.value;
-      var mensagem = this.mensagemInput.value;
-      this.tituloInput.value = '';
-      this.mensagemInput.value = '';
-      this.recados.push({
-        id: id,
-        titulo: titulo,
-        mensagem: mensagem
-      });
-      this.criarRecados();
-      this.savarLocalstore();
-    }
-  }, {
-    key: "deletaMenssagem",
-    value: function deletaMenssagem(event) {
-      if (!confirm('Deseja realmente apagar esta mensagem?')) return;
-      event.path[2].remove();
-      var scrapId = event.path[2].getAttribute('id-scrap');
-      var scrapIndex = this.recados.findIndex(function (item) {
-        return item.id == scrapId;
-      });
-      this.recados.splice(scrapIndex, 1);
-      this.savarLocalstore();
-    }
-  }, {
-    key: "inserirHtml",
-    value: function inserirHtml(html) {
-      this.caixaRecados.innerHTML += html;
-    }
-  }, {
-    key: "editaRecado",
-    value: function editaRecado(event) {
-      var _this3 = this;
-
-      $('#editModal').modal('toggle');
-      var scrapId = event.path[2].getAttribute('id-scrap');
-      var scrapIndex = this.recados.findIndex(function (item) {
-        return item.id == scrapId;
-      });
-      this.editTexto.value = this.recados[scrapIndex].titulo;
-      this.editMessagem.value = this.recados[scrapIndex].mensagem;
-
-      this.salveedit.onclick = function () {
-        return _this3.editSalvar(scrapIndex);
-      };
-    }
-  }, {
-    key: "editSalvar",
-    value: function editSalvar(scrapIndex) {
-      if (!confirm('Você realmente deseja salvar esta mensagem?')) return;
-      alert('Mensagem salva com sucesso!');
-      $("#editModal").modal("hide");
-      this.recados[scrapIndex].titulo = this.editTexto.value;
-      this.recados[scrapIndex].mensagem = this.editMessagem.value;
-      this.criarRecados();
-      this.savarLocalstore();
-    }
-  }, {
-    key: "savarLocalstore",
-    value: function savarLocalstore() {
-      localStorage.setItem('recados', JSON.stringify(this.recados));
-    }
-  }, {
-    key: "criaCartaoMensagem",
-    value: function criaCartaoMensagem(id, titulo, mensagem) {
-      return "\n        <div class=\"message-cards card text-white bg-dark m-2 col-3\" id-scrap=\"".concat(id, "\">\n        <div class=\"card-header font-weight-bold\">").concat(titulo, "</div>\n        <div class=\"card-body\">\n          <p class=\"card-text\">\n            ").concat(mensagem, "\n          </p>\n        </div>\n        <div class=\"w-100 d-flex justify-content-end pr-2 pb-2\">\n          <button class=\"btn btn-danger mr-1 delete-button\" \n          >Apagar</button>\n          <button class=\"btn btn-info editar-button \">Editar</button>\n        </div>\n      </div>\n      ");
-    }
-  }]);
-
-  return TaskList;
-}();
-
-new TaskList();
+/******/ });
--- src/index.js
+++ src/index.js
@@ -1,134 +1,138 @@
-class TaskList{
-    constructor(){
-        this.tituloInput = document.getElementById('titleInput');
-        this.mensagemInput = document.getElementById('messageField');
-        this.adicionar = document.getElementById('bnt');
-        this.caixaRecados= document.getElementById('caixa-recados');
-        this.editTexto= document.getElementById('editTitleInput');
-        this.editMessagem = document.getElementById('editMessageField');
-        this.salveedit= document.getElementById('saveEdit');
+// class TaskList{
+//     constructor(){
+//         this.tituloInput = document.getElementById('titleInput');
+//         this.mensagemInput = document.getElementById('messageField');
+//         this.adicionar = document.getElementById('bnt');
+//         this.caixaRecados= document.getElementById('caixa-recados');
+//         this.editTexto= document.getElementById('editTitleInput');
+//         this.editMessagem = document.getElementById('editMessageField');
+//         this.salveedit= document.getElementById('saveEdit');
         
         
-       this.recados= JSON.parse(localStorage.getItem("recados")) || [];
-       this.criarRecados();
-       this.regitaAddbtnEvent();
-    }
+//        this.recados= JSON.parse(localStorage.getItem("recados")) || [];
+//        this.criarRecados();
+//        this.regitaAddbtnEvent();
+//     }
     
-    generateRecadoId(){
-      return this.recados.length +1;
-    }
+//     generateRecadoId(){
+//       return this.recados.length +1;
+//     }
 
-    regitaAddbtnEvent(){
-        this.adicionar.onclick= () => this.novaMensagem();
+//     regitaAddbtnEvent(){
+//         this.adicionar.onclick= () => this.novaMensagem();
 
-    }
+//     }
 
-    botaoEvento(){
+//     botaoEvento(){
       
-      document.querySelectorAll('.delete-button').forEach((item)=>{item.onclick=(event)=>this.deletaMenssagem(event);
-      });
+//       document.querySelectorAll('.delete-button').forEach((item)=>{item.onclick=(event)=>this.deletaMenssagem(event);
+//       });
 
-      document.querySelectorAll('.editar-button').forEach(item=>{item.onclick = event=> this.editaRecado(event);
-      });
+//       document.querySelectorAll('.editar-button').forEach(item=>{item.onclick = event=> this.editaRecado(event);
+//       });
 
-    }
+//     }
 
-    criarRecados(){
-        this.caixaRecados.innerHTML ="";
+//     criarRecados(){
+//         this.caixaRecados.innerHTML ="";
 
-        for (const recado of this.recados) {         
-        const cardHtml = this.criaCartaoMensagem(
-        recado.id,
-        recado.titulo,
-        recado.mensagem);
-        this.inserirHtml(cardHtml);
-      }
-      this.botaoEvento();
-     }
+//         for (const recado of this.recados) {         
+//         const cardHtml = this.criaCartaoMensagem(
+//         recado.id,
+//         recado.titulo,
+//         recado.mensagem);
+//         this.inserirHtml(cardHtml);
+//       }
+//       this.botaoEvento();
+//      }
 
-    novaMensagem(){
-        if(!this.tituloInput.value || !this.mensagemInput.value){
-          alert('O titulo e a mensagem deve se digitadas!')
-          return;} 
-        const id = this.generateRecadoId();
-        const titulo = this.tituloInput.value;
-        const mensagem = this.mensagemInput.value;
+//     novaMensagem(){
+//         if(!this.tituloInput.value || !this.mensagemInput.value){
+//           alert('O titulo e a mensagem deve se digitadas!')
+//           return;} 
+//         const id = this.generateRecadoId();
+//         const titulo = this.tituloInput.value;
+//         const mensagem = this.mensagemInput.value;
       
-        this.tituloInput.value = '';
-        this.mensagemInput.value= '';
-        this.recados.push({id,titulo,mensagem});
+//         this.tituloInput.value = '';
+//         this.mensagemInput.value= '';
+//         this.recados.push({id,titulo,mensagem});
       
-        this.criarRecados();
-        this.savarLocalstore();
+//         this.criarRecados();
+//         this.savarLocalstore();
        
-      }
+//       }
 
-      deletaMenssagem(event){
-       if(!confirm('Deseja realmente apagar esta mensagem?'))return;
-        event.path[2].remove()
-      const scrapId = event.path[2].getAttribute('id-scrap');
+//       deletaMenssagem(event){
+//        if(!confirm('Deseja realmente apagar esta mensagem?'))return;
+//         event.path[2].remove()
+//       const scrapId = event.path[2].getAttribute('id-scrap');
     
-      const scrapIndex =this.recados.findIndex(item=>{
-        return item.id == scrapId;
-      })
+//       const scrapIndex =this.recados.findIndex(item=>{
+//         return item.id == scrapId;
+//       })
       
-      this.recados.splice(scrapIndex, 1);
-      this.savarLocalstore();
-      }
+//       this.recados.splice(scrapIndex, 1);
+//       this.savarLocalstore();
+//       }
      
-      inserirHtml(html){
-        this.caixaRecados.innerHTML += html
-      }
+//       inserirHtml(html){
+//         this.caixaRecados.innerHTML += html
+//       }
 
-      editaRecado(event) {
-        $('#editModal').modal('toggle');
-        const scrapId = event.path[2].getAttribute('id-scrap');
-        const scrapIndex = this.recados.findIndex((item) => {
-          return item.id == scrapId
-        })
+//       editaRecado(event) {
+//         $('#editModal').modal('toggle');
+//         const scrapId = event.path[2].getAttribute('id-scrap');
+//         const scrapIndex = this.recados.findIndex((item) => {
+//           return item.id == scrapId
+//         })
 
-         this.editTexto.value = this.recados[scrapIndex].titulo;
-         this.editMessagem.value = this.recados[scrapIndex].mensagem;
+//          this.editTexto.value = this.recados[scrapIndex].titulo;
+//          this.editMessagem.value = this.recados[scrapIndex].mensagem;
 
-        this.salveedit.onclick = () => this.editSalvar(scrapIndex) 
+//         this.salveedit.onclick = () => this.editSalvar(scrapIndex) 
 
-       }
+//        }
 
-       editSalvar(scrapIndex){
-        if(!confirm('Você realmente deseja salvar esta mensagem?')) return;
-        alert('Mensagem salva com sucesso!')
-        $("#editModal").modal("hide");
+//        editSalvar(scrapIndex){
+//         if(!confirm('Você realmente deseja salvar esta mensagem?')) return;
+//         alert('Mensagem salva com sucesso!')
+//         $("#editModal").modal("hide");
   
-       this.recados[scrapIndex].titulo = this.editTexto.value;
-       this.recados[scrapIndex].mensagem = this.editMessagem.value ;
-       this.criarRecados();
-       this.savarLocalstore();
-       }
+//        this.recados[scrapIndex].titulo = this.editTexto.value;
+//        this.recados[scrapIndex].mensagem = this.editMessagem.value ;
+//        this.criarRecados();
+//        this.savarLocalstore();
+//        }
 
-       savarLocalstore(){
-        localStorage.setItem('recados',JSON.stringify(this.recados));
-      }
+//        savarLocalstore(){
+//         localStorage.setItem('recados',JSON.stringify(this.recados));
+//       }
       
 
 
     
 
-    criaCartaoMensagem(id, titulo,mensagem){
-        return`
-        <div class="message-cards card text-white bg-dark m-2 col-3" id-scrap="${id}">
-        <div class="card-header font-weight-bold">${titulo}</div>
-        <div class="card-body">
-          <p class="card-text">
-            ${mensagem}
-          </p>
-        </div>
-        <div class="w-100 d-flex justify-content-end pr-2 pb-2">
-          <button class="btn btn-danger mr-1 delete-button" 
-          >Apagar</button>
-          <button class="btn btn-info editar-button ">Editar</button>
-        </div>
-      </div>
-      `;}
-}
+//     criaCartaoMensagem(id, titulo,mensagem){
+//         return`
+//         <div class="message-cards card text-white bg-dark m-2 col-3" id-scrap="${id}">
+//         <div class="card-header font-weight-bold">${titulo}</div>
+//         <div class="card-body">
+//           <p class="card-text">
+//             ${mensagem}
+//           </p>
+//         </div>
+//         <div class="w-100 d-flex justify-content-end pr-2 pb-2">
+//           <button class="btn btn-danger mr-1 delete-button" 
+//           >Apagar</button>
+//           <button class="btn btn-info editar-button ">Editar</button>
+//         </div>
+//       </div>
+//       `;}
+// }
 
-new TaskList();
+// new TaskList();
+
+import {soma}from './soma';
+
+alert(soma(1,20))
